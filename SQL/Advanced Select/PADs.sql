81b9c9386831b5656000326ac0bfd5daf7305a14
David <hoanluannguyen@gmail.com>
2017-05-01 16:57:57 -0500

    ll

--- SQL/Advanced Select/PADs.sql
+++ SQL/Advanced Select/PADs.sql
@@ -14,15 +14,13 @@ Note: There will be at least two entries in the table for each type of occupatio
 
 
 
-SELECT * FROM (
-  SELECT CONCAT(name, '(', SUBSTRING(occupation, 1, 1), ')')
-  FROM occupations
-  ORDER BY name ASC
-) temp1
-UNION
-SELECT * FROM (
-  SELECT concat('There are total ', count(*), ' ', LOWER(occupation), 's.')
-  FROM occupations
-  GROUP BY occupation
-  ORDER BY COUNT(*)
-) temp2
+
+
+select concat(Name, '(', SUBSTRING(Occupation, 1, 1), ')')
+from OCCUPATIONS
+order by Name;
+
+select concat('There are total ', count(*), ' ', lower(Occupation), 's.')
+from OCCUPATIONS
+group by Occupation
+order by count(*) ASC, Occupation ASC;
